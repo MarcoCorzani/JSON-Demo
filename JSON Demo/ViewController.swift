152d364ac6a3fe7ba73e11d9b7e22ac6b2dd2971
Marco <Marco@Marcos-iMac.fritz.box>
2015-02-04 13:18:32 +0100

    Start

--- JSON Demo/ViewController.swift
+++ JSON Demo/ViewController.swift
@@ -13,12 +13,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    
+        let urlPath = "http://www.telize.com/geoip"
+        
+        let url = NSURL(string: urlPath)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(url!, completionHandler: { data, response, error -> Void in
+           
+            if (error != nil) {
+                println(error)
+            } else {
+                //println(data)
+                
+             let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                
+                //println(jsonResult)
+                
+                
+                
+                println(jsonResult["city"])
+                
+               
+                
+            }
+        })
+        task.resume()
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
